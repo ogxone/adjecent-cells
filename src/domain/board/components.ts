c7b138c663f521a343c0e8b6103f14b870434b47
Valentin Yelchenko <valentin.elchenko@motorsport.com>
2022-01-30 23:43:49 +0200

    Board.loadCells

--- src/domain/board/components.ts
+++ src/domain/board/components.ts
@@ -1,26 +1,52 @@
-class Cell {
-    constructor(private position: Position, private color: Color) {}
+export class Cell {
+  constructor(private position: Position, private color: Color) {}
 }
 
-type Cells = Array<Array<Cell>>
+export type Cells = Array<Array<Cell>>
+type CellColors = Array<Array<Color>>;
 
 export class Board {
-    constructor(private cells: Cells) {}
+  private cells: Cells
+  private size: Size
+
+  constructor(cellColors: CellColors) {
+    [this.cells, this.size] = this.loadCells(cellColors);
+  }
+
+  private loadCells(cellColors: CellColors): [Cells, Size] {
+    let cells = [];
+    let [xDim, yDim] = [undefined, 0];
+    cellColors.forEach((row, yPos) => {
+      let cellsRow = [];
+      row.forEach((cellColor, xPos) => {
+        cellsRow.push(new Cell({ x: xPos, y: yPos }, cellColor));
+      });
+      if (xDim != undefined && cellsRow.length !== xDim) {
+        throw 'Invalid cells dimensions'
+      }
+      xDim = cellsRow.length
+      cells.push(cellsRow);
+    });
+
+    return [cells, {xDim, yDim: cells.length}]
+  }
 }
+
+type CellMasks = Array<Array<boolean>>;
 
 export class BoardMask {
-    constructor(private size: Size) {}
+  constructor(private cellMasks: CellMasks) {}
 }
 
-enum Color {
-    blue,
-    green,
-    red,
-    yellow,
-    brown,
-    cyan,
-    grey
+export enum Color {
+  blue,
+  green,
+  red,
+  yellow,
+  brown,
+  cyan,
+  grey,
 }
 
-export type Size = {xDim: number, yDim: number}
-export type Position = {x: number, y: number}
+export type Size = { xDim: number; yDim: number };
+export type Position = { x: number; y: number };
